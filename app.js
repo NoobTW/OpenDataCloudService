b5522b2e32fc8d0adc09f71cda07b68a2768f87d
lufor129 <s59654655@gmail.com>
2018-10-27 21:40:40 +0800

    third change

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var bodyParser = require("body-parser");
 
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -21,7 +22,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
-
 
 const request = require("request");
 const csv = require('csvtojson');
@@ -54,6 +54,35 @@ let Countys = {
   "連江":"22_lianjiang",
 }
 
+let blacklist = [
+    "臺北市",
+    "北市",
+    "新北市",
+    "桃園市",
+    "新竹市",
+    "臺南市",
+    "宜蘭縣",
+    "新竹縣",
+    "臺中市",
+    "高雄市",
+    "金門縣",
+    "南投縣",
+    "嘉義市",
+    "澎湖縣",
+    "臺東縣",
+    "屏東縣",
+    "基隆市",
+    "苗栗縣",
+    "彰化縣",
+    "雲林縣",
+    "嘉義縣",
+    "花蓮縣",
+    "連江縣",
+]
+
+var stack = [];
+var secondKey = [];
+let total = [];
 let text = fs.readFileSync("./rel2.txt",'utf8');
 let relateData = {};
 text.split(/\n/).forEach(line=>{
@@ -64,6 +93,28 @@ text.split(/\n/).forEach(line=>{
   }
   relateData[temp[0]] = temp2;
 })
+
+
+function getTotal(){
+  let promises=[];
+  for(element in Countys){
+    promises.push(
+      csv().fromFile(`./DataSet/${Countys[element]}.csv`).then((jsonObj)=>{
+        let temp = jsonObj;
+        temp.forEach((item)=>{
+          let cuts = nodejieba.cut(item["資料集名稱"]);
+          total.push(cuts);
+        })
+      })
+    )
+  }
+  Promise.all(promises).then(()=>{
+    console.log("finish total");
+    console.log(total.length);
+  })
+}
+
+getTotal();
 
 app.all('*', (req, res, next) => {
   res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization, Accept,X-Requested-With,x-csrf-token');
@@ -78,67 +129,165 @@ app.all('*', (req, res, next) => {
   }
 });
 
+// app.post("/data/county",function(req,res){
+//   let promises=[];
+//   let dict = {};
+//   let wordsets=[];
+//   let countys = req.body.data;
+//   let key = req.body.key;
+//   console.log(countys+" || "+key);
+//   let secondSearch={};
+//   if(countys.length == 0){
+//     for(index in Countys){
+//       countys.push(index);
+//     }
+//   }
+//   for(element in countys){
+//     promises.push(
+//       csv().fromFile(`./DataSet/${Countys[countys[element]]}.csv`).then((jsonObj)=>{
+//         let temp = jsonObj;
+//         temp.forEach((item)=>{
+//           let cuts = nodejieba.cut(item["資料集名稱"]);
+//           cuts.forEach(it=>{
+//             if(it.length>=2){
+//               if(dict[it]!=null){
+//                 dict[it]++;
+//               }else{
+//                 dict[it]=1;
+//               }
+//             }
+//           })
+//           if(cuts.includes(key)){
+//             cuts.forEach(it=>{
+//               if(it.length>=2){
+//                 if(secondSearch[it]!=null){
+//                   secondSearch[it]++;
+//                 }else{
+//                   secondSearch[it]=1;
+//                 }
+//               }
+//             })
+//           }
+//         })
+//       })
+//     )
+//   }
+//   Promise.all(promises).then(()=>{
+//     if(key==undefined){
+//       for(item in dict){
+//         if(dict[item]>100 && dict[item]<1000){
+//           wordsets.push([item,dict[item]]);
+//         }
+//       }
+//       res.send(wordsets);
+//     }else{
+//       for(item in secondSearch){
+//         if(secondSearch[item]>5){
+//           wordsets.push([item,secondSearch[item]]);
+//         }
+//       }
+//       res.send(wordsets);
+//     }
+//   })
+// })
+
 app.post("/data/county",function(req,res){
   let promises=[];
   let dict = {};
   let wordsets=[];
   let countys = req.body.data;
+  let relate = req.body.relate;
   let key = req.body.key;
+  let back = req.body.back;
+  let begintemp = [];
   console.log(countys+" || "+key);
-  let secondSearch={};
   if(countys.length == 0){
     for(index in Countys){
       countys.push(index);
     }
   }
-  for(element in countys){
-    promises.push(
-      csv().fromFile(`./DataSet/${Countys[countys[element]]}.csv`).then((jsonObj)=>{
-        let temp = jsonObj;
-        temp.forEach((item)=>{
-          let cuts = nodejieba.cut(item["資料集名稱"]);
-          cuts.forEach(it=>{
-            if(it.length>=2){
-              if(dict[it]!=null){
-                dict[it]++;
-              }else{
-                dict[it]=1;
-              }
+  if(relate == true){
+    stack = [];
+    stack.push(total);
+    secondKey = [];
+  }
+
+  if(back !=0){
+    for(let i=0;i<back;i++){
+      stack.pop();
+      secondKey.pop();
+    }
+  }
+  if(key != undefined && key!='縣市'){
+    secondKey.push(key);
+    let stackData = stack[stack.length-1];
+    for(index in stackData){
+      let temp = stackData[index];
+      if(temp.includes(key)){
+        begintemp.push(temp);
+        temp.forEach(it=>{
+          if(it.length>1){
+            if(dict[it]!=null){
+              dict[it]++;
+            }else{
+              dict[it]=1;
             }
-          })
-          if(cuts.includes(key)){
+          }
+        })
+      }
+    }
+  }else{
+    stack = [];
+    secondKey = [];
+    for(element in countys){
+      promises.push(
+        csv().fromFile(`./DataSet/${Countys[countys[element]]}.csv`).then((jsonObj)=>{
+          let temp = jsonObj;
+          temp.forEach((item)=>{
+            let cuts = nodejieba.cut(item["資料集名稱"]);
+            begintemp.push(cuts);
             cuts.forEach(it=>{
-              if(it.length>=2){
-                if(secondSearch[it]!=null){
-                  secondSearch[it]++;
+              if(it.length>1){
+                if(dict[it]!=null){
+                  dict[it]++;
                 }else{
-                  secondSearch[it]=1;
+                  dict[it]=1;
                 }
               }
             })
-          }
+          })
         })
-      })
-    )
+      )
+    }
   }
   Promise.all(promises).then(()=>{
-    if(key==undefined){
+    console.log(Object.keys(dict).length);
+    if(Object.keys(dict).length>2000){
       for(item in dict){
-        if(dict[item]>100 && dict[item]<1000){
+        if(dict[item]>100 && dict[item]<1000 && !blacklist.includes(item)){
           wordsets.push([item,dict[item]]);
         }
       }
-      res.send(wordsets);
-    }else{
-      for(item in secondSearch){
-        if(secondSearch[item]>5){
-          wordsets.push([item,secondSearch[item]]);
+     }else if(Object.keys(dict).length>600){ 
+      for(item in dict){
+        if(dict[item]>50 && dict[item]<500 && !blacklist.includes(item)){
+          wordsets.push([item,dict[item]]);
         }
       }
-      res.send(wordsets);
+    }else{
+      for(item in dict){
+        if(dict[item]>5 && !blacklist.includes(item)){
+          wordsets.push([item,dict[item]]);
+        }
+      }
     }
+    stack.push(begintemp);
+    console.log(secondKey);
+    console.log(stack.length);
+    res.send({data:wordsets,key:secondKey});
   })
 })
+
 
 app.post("/data/keyword",function(req,res){
   let key = req.body.key;
@@ -164,7 +313,10 @@ app.post("/data/search",function(req,res){
         let temp = jsonObj;
         temp.forEach((item)=>{
           let cuts = nodejieba.cut(item["資料集名稱"]);
-          if(cuts.includes(search)){
+          let flag = search.every(x=>{
+            return cuts.includes(x);
+          });
+          if(flag){
             result.push(item);
             if(dict[item["資料來源(部會單位)"]]!=undefined){
               dict[item["資料來源(部會單位)"]]++;
